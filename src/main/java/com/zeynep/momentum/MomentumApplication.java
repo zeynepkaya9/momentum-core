c82115ede83c42a8961183b07698732149063b76
zeynepkaya9 <zeynepkaya1723@gmail.com>
2026-06-23 00:00:14 +0300

    Initial project setup

---
+++ src/main/java/com/zeynep/momentum/MomentumApplication.java
@@ -0,0 +1,13 @@
+package com.zeynep.momentum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zeynep/momentum/MomentumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeynep.momentum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

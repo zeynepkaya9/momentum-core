3ec5af546800a9fc40e5d660aa9e691c081cd2ed
zeynepkaya9 <zeynepkaya1723@gmail.com>
2026-06-24 16:35:37 +0300

    Handled exception messages

--- src/main/java/com/zeynep/momentum/exception/GlobalExceptionHandler.java
+++ src/main/java/com/zeynep/momentum/exception/GlobalExceptionHandler.java
@@ -5,10 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 
 import java.util.HashMap;
 import java.util.Map;
+
 
 
 @RestControllerAdvice
@@ -31,5 +33,30 @@ public class GlobalExceptionHandler {
         return error;
     }
 
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String,String> handleValidationErrors(
+            MethodArgumentNotValidException exception
+    ){
+
+        Map<String,String> errors = new HashMap<>();
+
+
+        exception.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> {
+
+                    errors.put(
+                            error.getField(),
+                            error.getDefaultMessage()
+                    );
+
+                });
+
+
+        return errors;
+    }
 
 }
